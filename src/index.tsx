aa85e099d27c43d94b8e8c0e565c9ca469ad491f
Mitchel Pigsley <mpigsley@gmail.com>
2019-01-02 12:36:38 -0600

    Eslint & Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>Hello</h1>, document.getElementById('root')
-);
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
