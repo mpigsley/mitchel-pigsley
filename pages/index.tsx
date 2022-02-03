b295a0d91acf599ee0af20cb2235d41e900d18cc
Mitchel Pigsley <mpigsley@pm.me>
2022-02-03 10:59:19 -0600

    Add da face

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import Link from "next/link";
+import Image from "next/image";
 
 import AppWrapper from "components/app-wrapper";
 
+import Me from "public/mitch.png";
+
 const randomInterval = () => Math.round(Math.random() * 120 + 50);
-const TAG = "Mitchel Pigsley";
+const TAG = " Mitchel Pigsley ";
 
 export default function Home() {
   const [index, setIndex] = useState(0);
@@ -17,26 +19,35 @@ export default function Home() {
     }, randomInterval());
   }, [index, setIndex]);
 
-  const typingComplete = index === TAG.length;
-
   return (
     <AppWrapper>
       <div className="container">
+        <Image
+          src={Me}
+          alt="Mitchel Pigsley"
+          width="150"
+          height="150"
+          placeholder="blur"
+        />
         <div className="stage">{TAG.slice(0, index)}</div>
       </div>
       <style jsx>{`
         .container {
+          display: flex;
           height: 100vh;
-          position: relative;
           width: 100vw;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
         }
 
         .stage {
+          color: rgba(0, 0, 0, 0.6);
           display: flex;
           flex-direction: column;
-          font-size: 4rem;
+          font-size: 2rem;
+          height: 70px;
           justify-content: center;
-          height: 100%;
           text-align: center;
           width: 100%;
         }
